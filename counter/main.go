9941847b1545b9194d9ae437726b340a44d2f56e
AlexPassalis <alexpassalis9@gmail.com>
2025-06-24 22:28:47 +0300

    Table Driven Testing. Met the limits of the poor algorith.

--- counter/main.go
+++ counter/main.go
@@ -18,6 +18,7 @@ func CountWords(data []byte) int {
 		return 0
 	}
 
+	wordDetected := false
 	wordCount := 0
 
 	const spaceCharRune = ' ' // or const spaceCharASCII = 32 where 32 is the ASCII value of the Space character
@@ -25,7 +26,13 @@ func CountWords(data []byte) int {
 	for _, x := range data {
 		if x == spaceCharRune {
 			wordCount++ // increase the value of wordCount by 1, every time x is a space character (incorrect algorithm for many edge cases)
+		} else {
+			wordDetected = true
 		}
+	}
+
+	if !wordDetected { // if only spaces (0 words detected), return 0
+		return 0
 	}
 
 	wordCount++
