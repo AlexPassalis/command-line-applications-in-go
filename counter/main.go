c65c781e2bf710dcc67eae09119d6c87ddc0e0e1
AlexPassalis <alexpassalis9@gmail.com>
2025-06-24 23:31:58 +0300

    Last save before moving to the strings.Fields() function

--- counter/main.go
+++ counter/main.go
@@ -14,28 +14,18 @@ func main() {
 }
 
 func CountWords(data []byte) int {
-	if len(data) == 0 { // guard clause to prevent the edge case of an empty document
-		return 0
-	}
-
-	wordDetected := false
 	wordCount := 0
 
-	const spaceCharRune = ' ' // or const spaceCharASCII = 32 where 32 is the ASCII value of the Space character
-
+	wasSpace := true
 	for _, x := range data {
-		if x == spaceCharRune {
-			wordCount++ // increase the value of wordCount by 1, every time x is a space character (incorrect algorithm for many edge cases)
-		} else {
-			wordDetected = true
+		isSpace := (x == ' ' || x == '\n')
+
+		if wasSpace && !isSpace {
+			wordCount++
 		}
-	}
 
-	if !wordDetected { // if only spaces (0 words detected), return 0
-		return 0
+		wasSpace = isSpace
 	}
-
-	wordCount++
 
 	return wordCount
 }
