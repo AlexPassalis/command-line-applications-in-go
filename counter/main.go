ae0639b78afce2f2225bde8c8fcca21c0e5d9fc5
AlexPassalis <alexpassalis9@gmail.com>
2025-06-25 18:04:18 +0300

    Added the functionality of counting the words for multiple files by passing the name of the files as command line arguments.

--- counter/main.go
+++ counter/main.go
@@ -9,15 +9,31 @@ import (
 )
 
 func main() {
-	filename := "./words.txt"
+	log.SetFlags(0) // clears all log built-in prefixes.
+
+	if len(os.Args) < 2 {
+		log.Fatalln("error: no filename provided")
+	}
+
+	total := 0
+
+	filenames := os.Args[1:]
+	for _, filename := range filenames {
+		wordCount := CountWordsInFile(filename)
+		total += wordCount
+		fmt.Println(wordCount, filename)
+	}
+
+	fmt.Println(total, "words")
+}
+
+func CountWordsInFile(filename string) int {
 	file, err := os.Open(filename)
 	if err != nil {
 		log.Fatalln("failed to read file:", err)
 	}
 
-	wordCount := CountWords(file)
-	fmt.Println(wordCount)
-
+	return CountWords(file)
 }
 
 func CountWords(file io.Reader) int {
