0027b7b5a0ead46bb97e6c6131c1b1200528b48d
AlexPassalis <alexpassalis9@gmail.com>
2025-06-25 11:24:11 +0300

    PrintFileContents function instead of using CountWords function

--- counter/main.go
+++ counter/main.go
@@ -9,13 +9,36 @@ import (
 
 func main() {
 	filename := "./words.txt"
-	data, err := os.ReadFile(filename)
+	file, err := os.Open(filename)
 	if err != nil {
 		log.Fatalln("failed to read file:", err)
 	}
 
-	wordCount := CountWords(data)
-	fmt.Println(wordCount)
+	PrintFileContents(file)
+
+	/*
+		wordCount := CountWords(file)
+		fmt.Println(wordCount)
+	*/
+}
+
+func PrintFileContents(file *os.File) {
+	const bufferSize = 4096
+	buffer := make([]byte, bufferSize)
+
+	totalSize := 0
+	for {
+		size, err := file.Read(buffer)
+		if err != nil {
+			break
+		}
+
+		totalSize += size
+
+		fmt.Print(string(buffer[:size]))
+	}
+
+	fmt.Println("total bytes read:", totalSize)
 }
 
 func CountWords(data []byte) int {
