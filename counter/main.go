a0ded50da12e3c7cffdd4180b99004a2d47c9f36
AlexPassalis <alexpassalis9@gmail.com>
2025-06-25 09:38:25 +0300

    Error handling for the os.ReadFile function

--- counter/main.go
+++ counter/main.go
@@ -2,15 +2,19 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 	"strings"
 )
 
 func main() {
-	data, _ := os.ReadFile("./words.txt")
+	filename := "./words.txt"
+	data, err := os.ReadFile(filename)
+	if err != nil {
+		log.Fatalln("failed to read file:", err)
+	}
 
 	wordCount := CountWords(data)
-
 	fmt.Println(wordCount)
 }
 
